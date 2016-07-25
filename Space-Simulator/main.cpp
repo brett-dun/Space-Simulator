b598028fab754d4a8ca3aa7112b4c45174c2a65b
Brett Duncan <brettd314@gmail.com>
2016-07-25 17:10:48 -0500

    Initial Commit

---
+++ Space-Simulator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Space-Simulator
+//
+//  Created by Brett Duncan on 7/25/16.
+//  Copyright © 2016 Brett James Duncan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
